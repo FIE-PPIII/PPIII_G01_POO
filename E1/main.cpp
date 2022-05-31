fc3e934c842764897a7b293d920059f5875dcc22
Juan Bautista Nemi <80529710+jnemi2@users.noreply.github.com>
2022-05-31 20:38:53 -0300

    ADD - E1

--- E1/main.cpp
+++ E1/main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
+#include "Car.h"
 
 using namespace std;
 
 int main()
 {
-    cout<<"Hello, world!"<<endl;
+    Car cars[5] = {Car(Tesla, 2021, "Elon"), Car(BMW_Group, 2019, "Bill Gates"),
+                  Car(Tesla, 2016, "Trump"), Car(Nissan, 2008, "Stephen Harper"),
+                  Car(Honda, 2009, "Macron")};
+    for (int i = sizeof(cars)/sizeof(Car); i >= 0; i--){
+        if (cars[i].getModel() > 2010){
+            Car car = cars[i];
+            cout<<"This "<<car.getBrand()<<" "<<car.getModel()<<" belongs to "<<car.getOwnerName()<<endl;
+        }
+    }
 
     return 0;
 }
